a69dcda24bed6d9313c09e7ddb44690b924d7094
Isaac Peña <info@ipenat.com>
2024-06-30 20:08:20 +0200

    Minor changes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=/Users/isaac/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/include:$PATH
+export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/include:$PATH
 
 export CGO_CFLAGS="-I/opt/homebrew/include"
 export CGO_LDFLAGS="-L/opt/homebrew/lib"
